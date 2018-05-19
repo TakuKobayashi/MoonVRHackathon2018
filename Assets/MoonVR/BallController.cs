ce23e2d5a04d7b0f298f819531a9cfaa0c94f0ad
taka0928eye <32562287+taka0928eye@users.noreply.github.com>
2018-05-20 00:42:08 +0900

    Add files via upload

---
+++ Assets/MoonVR/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    public float speed = 1000;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        
+        var movement = new Vector3(0, 0.3f, 2);
+        
+        rb.AddForce(movement * speed);
+
+    }
+
+    void Update()
+    {
+    }
+}
