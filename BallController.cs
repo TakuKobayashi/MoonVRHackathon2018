4e495d349c4f8b34fc4fd800a728c39871f3bf33
taka0928eye <32562287+taka0928eye@users.noreply.github.com>
2018-05-20 10:27:07 +0900

    Add files via upload

---
+++ BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    public float speed = 1000; // 動く速さ
+
+    private Rigidbody rb; // Rididbody
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        
+        var movement = new Vector3(0, 0.5f, 1);
+        
+        rb.AddForce(movement * speed);
+
+    }
+
+    void Update()
+    {
+    }
+}
